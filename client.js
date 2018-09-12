5defd5da4ae990fc0448c9477d8c3d2d0cf47294
Samuel Solberg <pauliesays65@gmail.com>
2018-09-12 13:42:47 -0500

    magic happening

--- client.js
+++ client.js
@@ -1,13 +1,31 @@
-console.log('js');
-
 let myApp = angular.module('MyApp', []); // defining myApp with angular module
+
+
 
 myApp.controller('WelcomeController', function () {
     let self = this;
 
-    self.message = 'Hello';
+    self.message = '';
+    self.pointyThings = [
+        {
+            name: 'knives', sharpness: 10, isPoisonous: false
+        },
+
+        {
+            name: 'wits', sharpness: 20, isPoisonous: true
+        },
+
+        {
+            name: 'sea urchins', sharpness: 5, isPoisonous: true
+        }
+    ];
     self.greet = function () {
-        console.log('Greetings!')
+        console.log(`Hello ${self.message}!!`);
+        // $('#outputList').empty();
+        // self.pointyThings.push(self.message);
+        // for (let value of self.pointyThings) {
+        //     $('#outputList').append(`<li>${value}</li>`);
+        // }
     }
 
- })
+})
