16bbc6e9ba3fc077548cf32af271a5e461b3914b
Samuel Solberg <pauliesays65@gmail.com>
2018-09-12 10:42:27 -0500

    input and button added

--- client.js
+++ client.js
@@ -1,9 +1,13 @@
 console.log('js');
 
-let myApp = angular.module('MyApp', []);
+let myApp = angular.module('MyApp', []); // defining myApp with angular module
 
 myApp.controller('WelcomeController', function () {
     let self = this;
 
     self.message = 'Hello';
-})
+    self.greet = function () {
+        console.log('Greetings!')
+    }
+
+ })
